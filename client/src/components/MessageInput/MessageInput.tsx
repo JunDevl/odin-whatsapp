89fe5eb892979a69f16244754e857362d6f92b23
JunDevl <juninhoplay.pedone@gmail.com>
2026-09-03 14:13:01 -0300

    Improved message input styling

--- client/src/components/MessageInput/MessageInput.tsx
+++ client/src/components/MessageInput/MessageInput.tsx
@@ -1,6 +1,6 @@
 import "./messageinput.css"
 
-import { useEffect, useRef, type InputEvent, type KeyboardEvent, type SubmitEvent } from "react";
+import { useEffect, useRef, useState, type InputEvent, type KeyboardEvent, type SubmitEvent } from "react";
 import { SelectedChatContext, type MessageResponse } from "../../utils";
 import { useContext } from "react";
 import { createMessage } from "../../actions";
@@ -12,6 +12,8 @@ type Props = {
 };
 
 const MessageInput = ({ kind }: Props) => {
+  const [text, setText] = useState("");
+
   const queryClient = useQueryClient();
 
   const form = useRef<HTMLFormElement>(null);
@@ -52,23 +54,28 @@ const MessageInput = ({ kind }: Props) => {
   }
 
   const handleInput = (e: InputEvent<HTMLTextAreaElement>) => {
-    return
+    const target = e.target as HTMLTextAreaElement;
+
+    setText(target.value);
+    // console.log(text);?
   }
 
   return (
-    <footer className="z-20 p-1 bg-transparent">
-      <form id="message_form" method="POST" onSubmit={onSubmitMessage} ref={form} className="bg-transparent">
-        <label className="flex bg-dark-300 rounded-full p-2" htmlFor="content">
-          <button id="send" className="p-2 px-3 w-10">
+    <footer className="z-20 px-1 pb-1" id="input-area">
+      <form id="message-form" method="POST" onSubmit={onSubmitMessage} ref={form} className="bg-dark-300 rounded-4xl has-[textarea:focus]:outline-1 has-[textarea:focus]:outline-light-900">
+        <label className="flex items-stretch p-0.5 gap-2" htmlFor="content">
+          <button id="send" className={`flex items-center p-2 px-3 size-10 rounded-4xl ${text !== "" ? "bg-primary-900" : ""}`}>
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M11.5003 12H5.41872M5.24634 12.7972L4.24158 15.7986C3.69128 17.4424 3.41613 18.2643 3.61359 18.7704C3.78506 19.21 4.15335 19.5432 4.6078 19.6701C5.13111 19.8161 5.92151 19.4604 7.50231 18.7491L17.6367 14.1886C19.1797 13.4942 19.9512 13.1471 20.1896 12.6648C20.3968 12.2458 20.3968 11.7541 20.1896 11.3351C19.9512 10.8529 19.1797 10.5057 17.6367 9.81135L7.48483 5.24303C5.90879 4.53382 5.12078 4.17921 4.59799 4.32468C4.14397 4.45101 3.77572 4.78336 3.60365 5.22209C3.40551 5.72728 3.67772 6.54741 4.22215 8.18767L5.24829 11.2793C5.34179 11.561 5.38855 11.7019 5.407 11.8459C5.42338 11.9738 5.42321 12.1032 5.40651 12.231C5.38768 12.375 5.34057 12.5157 5.24634 12.7972Z" stroke="currentColor" fill="transparent" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </button>
           <textarea 
-            className="min-w-0 flex-1 field-sizing-content"
+            className="min-w-0 flex-1 field-sizing-content focus:outline-0"
             name="content" 
             id="content" 
-            ref={input} 
+            ref={input}
+            value={text}
+            required={true}
             onInput={handleInput}
             onKeyDown={handleKeyDown}
           />
