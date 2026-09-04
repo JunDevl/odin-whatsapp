5c679d34771c08395fa173325d533b3283ef50fb
JunDevl <juninhoplay.pedone@gmail.com>
2026-09-04 20:51:25 -0300

    Improved styling and added some icons

--- client/src/components/ChatList/ChatList.tsx
+++ client/src/components/ChatList/ChatList.tsx
@@ -1,3 +1,4 @@
+import "./chatlist.css";
 import { Suspense, useContext, useRef } from "react";
 import type { ChatType, Contact } from "../../utils";
 import NewChatModal from "../NewChatModal/NewChatModal";
@@ -17,16 +18,25 @@ const ChatList = <T extends Contact | Group, >({ kind, chats }: Props<T>) => {
   const isGroupSelected = selectedChat && "group" in selectedChat!;
 
   return (
-    <nav id={`${kind}s-sidebar`} className="overflow-hidden">
+    <nav id={`${kind}s-sidebar`} className="overflow-hidden flex flex-col">
       <NewChatModal kind={kind} ref={newChatModal}/>
-      <div id="new_chat">
-        <button id={`add_${kind}`} onClick={() => newChatModal.current!.showModal()}>
-          {`${kind === "user" ? "Add" : "Create"} ${kind[0].toUpperCase()}${kind.slice(1)}`}
-        </button>
-      </div>
+      <button 
+        id={`add_${kind}`} 
+        onClick={() => newChatModal.current!.showModal()} 
+        className="new-chat flex h-10 gap-2 justify-center items-center hover:bg-primary-500 active:bg-primary-400 bg-dark-400 m-0.5 rounded-full"
+      >
+        {kind === "user" && 
+         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6">
+          <path d="M3 19C3.69137 16.6928 5.46998 16 9.5 16C13.53 16 15.3086 16.6928 16 19" stroke="currentColor" fill="none" strokeWidth="2" strokeLinecap="round"/>
+          <path d="M13 9.5C13 11.433 11.433 13 9.5 13C7.567 13 6 11.433 6 9.5C6 7.567 7.567 6 9.5 6C11.433 6 13 7.567 13 9.5Z" stroke="currentColor" fill="none" strokeWidth="2"/>
+          <path d="M15 6H21" stroke="currentColor" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M18 3L18 9" stroke="currentColor" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>}
+        <p>{`${kind === "user" ? "Add" : "Create"} ${kind[0].toUpperCase()}${kind.slice(1)}`}</p>
+      </button>
       <div className="list">
         <div id="search-chat" className="search m-2 bg-dark-500 h-8">
-          <input type="text" name="" id="" placeholder="Search Chat"/>
+          <input type="text" name="" id="" placeholder={`Search ${kind[0].toUpperCase()}${kind.slice(1)}`}/>
           <button className="search-icon">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M 10 2 C 5.5965257 2 2 5.5965291 2 10 C 2 14.403471 5.5965257 18 10 18 C 11.752132 18 13.370523 17.422074 14.691406 16.458984 L 19.845703 21.613281 A 1.250125 1.250125 0 1 0 21.613281 19.845703 L 16.458984 14.691406 C 17.422074 13.370523 18 11.75213 18 10 C 18 5.5965291 14.403474 2 10 2 z M 10 4.5 C 13.052375 4.5 15.5 6.947627 15.5 10 C 15.5 13.052373 13.052375 15.5 10 15.5 C 6.9476251 15.5 4.5 13.052373 4.5 10 C 4.5 6.947627 6.9476251 4.5 10 4.5 z"/>
--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -114,7 +114,7 @@ const Message = ({ message, user, index }: Props) => {
             selectArrowIcon.current!.classList.toggle("open");
           }}
         >
-          <ul className="detail p-1 flex *:flex *:h-10 flex-col items-stretch [&>li>button]:bg-dark-500 [&>li>button]:flex [&>li>button]:flex-1 [&>li>button]:justify-between [&>li>button]:items-center [&>li>button]:gap-2 [&>li>button]:hover:bg-dark-600 [&>li>button]:cursor-pointer [&>li>button>svg]:max-w-5 [&>li>button>p]:flex-1 [&>li>button>p]:text-left">
+          <ul className="detail overflow-hidden p-1 flex *:flex *:h-8 flex-col items-stretch [&>li>button]:bg-dark-500 [&>li>button]:flex [&>li>button]:flex-1 [&>li>button]:items-center [&>li>button]:gap-2 [&>li>button:not(.trash)]:hover:bg-dark-600 [&>li>button]:cursor-pointer [&>li>button>svg]:w-5 [&>li>button>p]:flex-1 [&>li>button>p]:text-left">
             {isOwn && <>
               <li>
                 <button className="pencil" onClick={openMessageModal}>
@@ -125,7 +125,7 @@ const Message = ({ message, user, index }: Props) => {
                 </button>
               </li>
               <li>
-                <button className="trash" onClick={handleDelete}>
+                <button className="trash hover:bg-semantic-error-300" onClick={handleDelete}>
                   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10 12L14 16M14 12L10 16M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M18 6V16.2C18 17.8802 18 18.7202 17.673 19.362C17.3854 19.9265 16.9265 20.3854 16.362 20.673C15.7202 21 14.8802 21 13.2 21H10.8C9.11984 21 8.27976 21 7.63803 20.673C7.07354 20.3854 6.6146 19.9265 6.32698 19.362C6 18.7202 6 17.8802 6 16.2V6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
                   </svg>
