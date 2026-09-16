7f85d9ea86217a7be3c0f2961dc821be55cbbc52
JunDevl <juninhoplay.pedone@gmail.com>
2026-09-15 23:13:17 -0300

    Finished context menu component and added an input for attaching files in a message

--- client/src/components/ContextMenu/ContextMenu.tsx
+++ client/src/components/ContextMenu/ContextMenu.tsx
@@ -1,37 +1,37 @@
-import type { ComponentProps, JSX, ReactElement, ReactNode, RefObject } from "react";
+import type { ComponentProps, JSX, ReactElement, ReactNode, RefObject, SVGProps } from "react";
 import "./contextmenu.css";
+import type React from "react";
+import { cn } from "../../utils";
 
 type MenuItemProps = {
-  name: string
-  children: ReactNode
-} & Omit<ComponentProps<"button">, "className">
+  name?: string
+  children?: ReactNode;
+} & ComponentProps<"button">
 
-const MenuItem = ({name, children, ...props}: MenuItemProps) => {
+export const MenuItem = ({name, children, className, ...props}: MenuItemProps) => {
   return <li>
-    <button className={name} {...props}>
+    <button className={cn(className, name)} {...props}>
       {children}
-      <p className="info">{`${name[0].toUpperCase()}${name.slice(1)}`}</p>
+      {name && <p className="info">{`${name[0].toUpperCase()}${name.slice(1)}`}</p>}
     </button>
   </li>
 }
 
 type ContextMenuProps = {
   ref: RefObject<HTMLDivElement | null>
-  children: ReactElement<MenuItemProps> | ReactElement<MenuItemProps>[]
-} & Omit<ComponentProps<"div">, "className" | "ref">
+  children: ReactElement<MenuItemProps> | (false | ReactElement<MenuItemProps>)[]
+} & Omit<ComponentProps<"div">, "ref">
 
-const ContextMenu = ({children, ...props}: ContextMenuProps) => {
+export const ContextMenu = ({children, className, ...props}: ContextMenuProps) => {
   return (
     <div 
       {...props}
-      className="message-menu bg-dark-500 z-10 rounded-2xl border border-dark-200 text-sm"
+      className={cn(className, "bg-dark-500 z-10 rounded-2xl border border-dark-200 text-sm")}
       popover="auto"
     >
-      <ul className="detail overflow-hidden p-1 flex *:flex *:h-8 flex-col items-stretch [&>li>button]:bg-dark-500 [&>li>button]:flex [&>li>button]:flex-1 [&>li>button]:items-center [&>li>button]:gap-2 [&>li>button:not(.trash)]:hover:bg-dark-600 [&>li>button]:cursor-pointer [&>li>button>svg]:w-5 [&>li>button>p]:flex-1 [&>li>button>p]:text-left">
+      <ul className="detail overflow-hidden p-1 flex *:flex flex-col items-stretch [&>li>button]:bg-dark-500 [&>li>button]:flex [&>li>button]:flex-1 [&>li>button]:items-center [&>li>button]:gap-2.5 [&>li>button:not(.trash)]:hover:bg-dark-600 [&>li>button]:cursor-pointer [&>li>button>svg]:w-5 [&>li>button>p]:flex-1 [&>li>button>p]:text-left">
         {children}
       </ul>
     </div>
   )
 }
-
-export default ContextMenu
